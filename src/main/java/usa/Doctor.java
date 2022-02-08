28ea671bb81119481424d432f80f43e5520f6b0b
SUBE007 <chaurasiyasube007@gmail.com>
2022-02-08 18:12:18 +0530

    initial commit

---
+++ src/main/java/usa/Doctor.java
@@ -0,0 +1,7 @@
+package usa;
+
+public class Doctor {
+    public void  assist(){
+        System.out.println("Doctor is Assisting");
+    }
+}
---
+++ src/main/java/usa/Main.java
@@ -0,0 +1,8 @@
+package usa;
+
+public class Main {
+    public static void main(String[] args) {
+        Doctor doctor=new Doctor();
+        doctor.assist();
+    }
+}
