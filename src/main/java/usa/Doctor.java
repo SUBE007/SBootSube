4742a17c8101a8859b38344db00afb8af41062e9
SUBE007 <chaurasiyasube007@gmail.com>
2022-02-08 19:18:00 +0530

    added doctor qualification and getter setter metod into spring.xml file

--- src/main/java/usa/Doctor.java
+++ src/main/java/usa/Doctor.java
@@ -1,6 +1,17 @@
 package usa;
 
 public class Doctor implements Staff{
+    private  String qualification;
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
+
     public void  assist(){
         System.out.println("Doctor is Assisting");
     }
--- src/main/java/usa/Main.java
+++ src/main/java/usa/Main.java
@@ -6,7 +6,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
-        Staff staff=context.getBean(Doctor.class);
+        //Staff staff=context.getBean(Doctor.class);
+       Doctor staff=context.getBean(Doctor.class);
         staff.assist();
+        System.out.println(staff.getQualification());
     }
 }
