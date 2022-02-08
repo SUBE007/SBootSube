bb99d7f17f115bee7bfd86715615e2a5e7d40b75
SUBE007 <chaurasiyasube007@gmail.com>
2022-02-08 18:50:10 +0530

    bcz spring does not know we have to create beans for this class

--- src/main/java/usa/Main.java
+++ src/main/java/usa/Main.java
@@ -1,8 +1,12 @@
 package usa;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        Doctor doctor=new Doctor();
+        ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+        Doctor doctor=context.getBean(Doctor.class);
         doctor.assist();
     }
 }
